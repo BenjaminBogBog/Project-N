fbd21e987ad485e31bdea3675a2f337ab5bf72d9
BenBogBog <benjaminbogbog@gmail.com>
2022-11-06 17:47:01 +0100

    Set up C++ Class for Player Char

--- Source/ProjectNGit/Private/PlayerChar.cpp
+++ Source/ProjectNGit/Private/PlayerChar.cpp
@@ -3,11 +3,38 @@
 
 #include "PlayerChar.h"
 
+
 // Sets default values
 APlayerChar::APlayerChar()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 600.0f;
+	GetCharacterMovement()->AirControl = 0.5f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	CameraBoom->TargetOffset = FVector(0, 0, 50.0);
+	CameraBoom->TargetArmLength = 450.0;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->SetFieldOfView(105.0);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	bDead = false;
 
 }
 
@@ -29,6 +56,68 @@ void APlayerChar::Tick(float DeltaTime)
 void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Vertical"), this, &APlayerChar::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Horizontal"), this, &APlayerChar::MoveRight);
+
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Released, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAction(TEXT("Sprint"), IE_Pressed, this, &APlayerChar::StartSprint);
+	PlayerInputComponent->BindAction(TEXT("Sprint"), IE_Released, this, &APlayerChar::StopSprint);
+
+	PlayerInputComponent->BindAction(TEXT("Attack"), IE_Pressed, this, &APlayerChar::Attack);
+
+}
+
+void APlayerChar::MoveForward(float Axis)
+{
+	if (!bDead)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void APlayerChar::MoveRight(float Axis)
+{
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(Direction, Axis);
+}
+
+void APlayerChar::StartSprint() {
+
+	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+}
+
+void APlayerChar::StopSprint() {
+
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+}
+
+void APlayerChar::Attack() {
+	PlayAnimMontage(attackAnim);
+}
+
+void APlayerChar::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *OtherActor->GetName());
+
+}
+
+void APlayerChar::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 
 }
 
--- Source/ProjectNGit/Public/PlayerChar.h
+++ Source/ProjectNGit/Public/PlayerChar.h
@@ -4,6 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+
 #include "PlayerChar.generated.h"
 
 UCLASS()
@@ -15,9 +26,41 @@ public:
 	// Sets default values for this character's properties
 	APlayerChar();
 
+	UFUNCTION()
+		void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		UCameraComponent* FollowCamera;
+
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
+
+	bool bDead;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float WalkSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float SprintSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UAnimMontage* attackAnim;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void StartSprint();
+	void StopSprint();
+
+	void Attack();
 
 public:	
 	// Called every frame
