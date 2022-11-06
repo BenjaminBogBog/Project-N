07e25207cc76ee58a1e9a3dd6132fb018c4209b5
BenBogBog <benjaminbogbog@gmail.com>
2022-11-06 14:03:16 +0100

    Generated UProject Files

---
+++ Source/ProjectNGit/ProjectNGitGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNGitGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNGIT_API AProjectNGitGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
